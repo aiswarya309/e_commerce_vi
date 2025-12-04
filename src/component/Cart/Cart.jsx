8a12a1f5929019c371f369a8da0a2953857619bc
Aiswarya <aishuzz309@gmail.com>
2025-12-04 10:50:29 +0530

    changes of price in cart

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import './cart.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { Navbar } from '../Navbar/Navbar'
 import { Footer } from '../Footer/Footer'
-import { decrement, increment, removeItem, saveforLater, removeSaved } from '../../redux/slice'
+import { decrement, increment, removeItem, saveforLater, removeSaved,addToCart,SaveCartDecrement,saveCartIncrement } from '../../redux/slice'
 // import { Modal } from '@mui/base/Modal';
 // import { Modal } from '@mui/material';
 import Modal from '@mui/material/Modal';
@@ -18,9 +18,26 @@ export const Cart = () => {
     const saveLater = useSelector((state) => state.cart.saveForLater)
     const dispatch = useDispatch();
     const [modalShow, setModalShow] = React.useState(false);
-    const { address} = useContext(AddressContext)
-    console.log("cart", cart);
+    const { address } = useContext(AddressContext)
+    const totalPrice = cart.reduce((total, item) => total + item.quantity * item.price, 0)
+    const totalDiscount = cart.reduce((discount, item) => discount + (item.discountPercentage * item.quantity) / 100, 0)
+    const totalItem = cart.reduce((total, item) => total + item.quantity, 0)
+    const platformFee = 20
+    const totalAmount = totalPrice - totalDiscount + platformFee
+    const saveMoney = totalAmount - totalPrice
+    console.log("cart", cart,totalPrice);
+    const getDeliveryDate = () => {
+        const today = new Date();
+        console.log("*",today.getDay());
+        today.setDate(today.getDay() + 7)
+        return today.toDateString();
 
+    }
+
+    const handleSaveLaterToCart =(item)=>{
+        dispatch(addToCart(item));
+        dispatch(removeSaved(item.id));
+    }
     return (<>
         <div className='container-cart'>
             <div className='cart'>
@@ -45,8 +62,8 @@ export const Cart = () => {
                                     onHide={() => setModalShow(false)}
                                 /></span>}
                     </div>
-}
-                    
+                    }
+
                 </div>
                 {
                     cart && cart.length ? cart.map((item, i) => (
@@ -68,7 +85,7 @@ export const Cart = () => {
                                 {/* <button className='' onClick={() => dispatch(removeItem(item.id))}>Remove</button> */}
                                 <RemoveCartPopUp id={item.id} />
                             </div>
-                            <div className="date">Delivery by Mon Oct 20</div>
+                            <div className="date">Delivery by {getDeliveryDate()}</div>
                         </div>
                     )) : <div className='empty-cart'>
                         <img src={cartEmpty} className='empty-cart-img' />
@@ -82,7 +99,7 @@ export const Cart = () => {
 
                 <div className="saveLater">
                     {
-                        saveLater && saveLater.length ? (
+                        saveLater && saveLater.length>0 ? (
                             <>
                                 <hr />
                                 <h3>Saved for later</h3>
@@ -92,9 +109,9 @@ export const Cart = () => {
                                         <div className="img">
                                             <img src={item.thumbnail} alt="" />
                                             <div className='cart-change'>
-                                                <button className='cart-btn' onClick={() => dispatch(decrement(item.id))}>-</button>
+                                                <button className='cart-btn' disabled>-</button>
                                                 <div>{item.quantity}</div>
-                                                <button className='cart-btn' onClick={() => dispatch(increment(item.id))}>+</button>
+                                                <button className='cart-btn' disabled>+</button>
                                             </div>
                                         </div>
                                         <div className="details">
@@ -103,6 +120,7 @@ export const Cart = () => {
                                             <p className="catagory">{item.category}</p>
                                             <p className="price">{item.price}</p>
                                             <button className='' onClick={() => dispatch(removeSaved(item.id))}>Remove</button>
+                                            <button className='' onClick={() => handleSaveLaterToCart(item)}>Add to cart</button>
                                         </div>
                                     </div>
                                 ))}
@@ -111,20 +129,20 @@ export const Cart = () => {
                 </div>
 
             </div>
-            <div className="price-details">
+            {cart.length >0 &&   <div className="price-details">
                 <div className="title">
                     <h3>PRICE DETAILS</h3>
                 </div>
                 <div className="price">
-                    <p>price</p>
-                    <p>Discount</p>
-                    <p>Coupons</p>
-                    <p>Platform fee</p><hr />
-                    <p><b>Total Amount</b></p><hr />
-                    <p>You will save $ 15,309 on this order</p>
+                    <p>price {`(${totalItem} item)`}</p>{totalPrice.toFixed(2)}
+                    <p>Discount {totalDiscount.toFixed(2)}</p>
+                    <p>Platform fee {platformFee}</p><hr />
+                    <p><b>Total Amount {totalAmount.toFixed(2)}</b></p><hr />
+                    <p>You will save $ {saveMoney.toFixed(2)} on this order</p>
 
                 </div>
-            </div>
+            </div>}
+          
         </div>
     </>
     )
